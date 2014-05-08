372df6e7fb9084baabb769d9b0f6e6fecb8f2b98
geoffreymcgill <geoff@ext.net>
2014-05-08 09:34:17 -0600

    Renamed /DateTime/ project to /Bridge.DateTime/

---
+++ Bridge.DateTime/DayOfWeek.cs
@@ -0,0 +1,45 @@
+﻿using Bridge.CLR;
+using Bridge.CLR.Html;
+using System;
+
+namespace System
+{
+    [Name("Bridge.DayOfWeek")]
+    public enum DayOfWeek
+    {
+        /// <summary>
+        /// Indicates Sunday.
+        /// </summary>
+        Sunday = 0,
+        
+        /// <summary>
+        /// Indicates Monday.
+        /// </summary>
+        Monday = 1,
+        
+        /// <summary>
+        /// Indicates Tuesday.
+        /// </summary>
+        Tuesday = 2,
+        
+        /// <summary>
+        /// Indicates Wednesday.
+        /// </summary>
+        Wednesday = 3,
+        
+        /// <summary>
+        /// Indicates Thursday.
+        /// </summary>
+        Thursday = 4,
+
+        /// <summary>
+        /// Indicates Friday.
+        /// </summary>
+        Friday = 5,
+
+        /// <summary>
+        /// Indicates Saturday.
+        /// </summary>
+        Saturday = 6
+    }
+}
