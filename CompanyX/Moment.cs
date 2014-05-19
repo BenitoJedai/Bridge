9cb3a7b3d6604a173920b92201c0921fbee09722
geoffreymcgill <geoff@ext.net>
2014-05-18 20:43:32 -0600

    Testing Moment library

---
+++ CompanyX/Moment.cs
@@ -0,0 +1,21 @@
+﻿using Bridge.CLR;
+using Bridge.CLR.Html;
+using System;
+
+namespace Moment
+{
+    [Namespace(false)]
+    [Name("moment")]
+    [Constructor("moment")]
+    public class Moment
+    {
+        public Moment()
+        {
+        }
+
+        public Moment AddYears(int value)
+        {
+            return new Moment();
+        }
+    }
+}
