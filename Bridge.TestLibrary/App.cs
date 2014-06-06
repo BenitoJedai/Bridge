c5b8654dbfc3ca83b5fed068efa345f09a2ec238
Vladimir Scheglov <vscheglov@gmail.com>
2014-06-06 13:57:58 +0400

    [UPDATE] Sandbox update

--- Bridge.TestLibrary/App.cs
+++ Bridge.TestLibrary/App.cs
@@ -8,43 +8,40 @@ namespace TestProject
 {    
     public partial class Class1
     {
-        public int field;
-
-        public void Method1()
+        public static void Method1(string s = "")
         {
-            var btn = Document.GetElementById("btn1");
-
-            if (btn == null)
-            {
-                Window.OnLoad = delegate(Event e) {
-                    this.Method1();
-                };
-
-                return;
-            }
-            
-            btn.OnClick = btn.OnClick + delegate(Event e) {
-                field = 0;
-                Window.Alert("click1");
-            };
-
-            btn.OnClick += e => Window.Alert("click3");
-
-            Action<Event> handler = delegate(Event e)
-            {
-                field = 0;
-                Window.Alert("click2");
-            };
-
-            btn.OnClick = btn.OnClick + handler;
-            btn.OnClick = btn.OnClick - handler;
+            Console.Log("%d", 1);
         }
-        
+
+        public static void Method1(int i, string s = "")
+        {
+        }
+
+        public static void Method1(params string[] args)
+        {
+            var l = args.Length;
+        }
+
+        public static void Method1(int i, params string[] args)
+        {
+            var l = args.Length;
+        }
+
+        public static int CalculateBMI(int weight, int height)
+        {
+            return (weight * 703) / (height * height);
+        }
         
         public static void Start()
         {
-            var c = new Class1();
-            c.Method1();
+            Method1();
+            Method1(0);
+            Method1("str");
+            Method1("1", "2", "3", "4");
+            Method1(0, "str");
+            Method1(0, "1", "2", "3", "4");
+
+            CalculateBMI(height: 64, weight: 123);
         }
     }
 }
--- Bridge.TestLibrary/Bridge.TestLibrary.js
+++ Bridge.TestLibrary/Bridge.TestLibrary.js
@@ -1,14 +1,26 @@
 Bridge.Class.extend('TestProject.Class1', {
     $statics: {
+        method1$Class1: function (c) {
+        },
+        method1$Int32$String: function (i, s) {
+        },
+        method1: function () {
+            if (arguments.length == 1) {
+                if (Bridge.is(arguments[0], TestProject.Class1)) {
+                    this.method1$Class1.apply(this, arguments);
+                }
+            }
+            else if (arguments.length == 2) {
+                if (Bridge.is(arguments[0], Number) && Bridge.is(arguments[1], String)) {
+                    this.method1$Int32$String.apply(this, arguments);
+                }
+            }
+        },
         start: function () {
-            var btn = document.getElementById("btn1");
-            btn.onclick += function (e) {
-                window.alert("click1");
-            };
-            btn.onclick = function (e) { return e.stopPropagation() };
-            btn.onclick += function (e) {
-                window.alert("click2");
-            };
+            var arr = [ ];
+            TestProject.Class1.method1$Class1();
+            TestProject.Class1.method1$Int32$String(0);
+            /* Method1("1", "2", "3", "4"); */
         }
     }
 });
