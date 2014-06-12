091847ae023fec7a1af199e6b0796b3eff905630
geoffreymcgill <geoff@ext.net>
2014-06-12 11:36:30 -0600

    Added double quotes around project and .bat file paths
    Signed-off-by: geoffreymcgill <geoff@ext.net>

--- Bridge.DateTime/build.bat
+++ Bridge.DateTime/build.bat
@@ -1,2 +1,2 @@
 SET _=%~dp0
-%_%..\assemblies\Bridge.Builder.exe -p %_%Bridge.DateTime.csproj -o %_%build\Bridge.DateTime.js
+"%_%..\assemblies\Bridge.Builder.exe" -p "%_%Bridge.DateTime.csproj" -o "%_%build\Bridge.DateTime.js"
