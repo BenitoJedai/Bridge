0c90dde441fe555a68362ad27d11d6b04e638dcd
geoffreymcgill <geoff@ext.net>
2014-06-25 00:07:17 -0600

    Added Array .IndexOf and .LastindexOf Methods
    Signed-off-by: geoffreymcgill <geoff@ext.net>

--- Core/Bridge.CLR/System/Array.cs
+++ Core/Bridge.CLR/System/Array.cs
@@ -29,6 +29,49 @@ namespace System
             return null; 
         }
 
+        /// <summary>
+        /// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+        /// </summary>
+        /// <param name="searchElement"></param>
+        /// <returns></returns>
+        public int IndexOf(string searchElement)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
+        /// </summary>
+        /// <param name="searchElement"></param>
+        /// <param name="fromIndex"></param>
+        /// <returns></returns>
+        public int IndexOf(string searchElement, int fromIndex)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// The lastIndexOf() method returns the last index at which a given element can be found in the array, or -1 if it is not present. The array is searched backwards, starting at fromIndex.
+        /// </summary>
+        /// <param name="searchString"></param>
+        /// <returns></returns>
+        public int LastIndexOf(string searchString)
+        {
+            return 0;
+        }
+
+        /// <summary>
+        /// The lastIndexOf() method returns the last index at which a given element can be found in the array, or -1 if it is not present. The array is searched backwards, starting at fromIndex.
+        /// </summary>
+        /// <param name="searchString"></param>
+        /// <param name="fromIndex"></param>
+        /// <returns></returns>
+        public int LastIndexOf(string searchString, int fromIndex)
+        {
+            return 0;
+        }
+
+
         public string Join(string separator) 
         { 
             return null; 
