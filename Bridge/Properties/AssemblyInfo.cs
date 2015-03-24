8032f67e91ee6f4e7567b5bdd2c12a170b655418
geoffreymcgill <geoff@object.net>
2015-03-23 20:41:55 -0600

    Added AssemblyInfo and set version number

--- Bridge/Properties/AssemblyInfo.cs
+++ Bridge/Properties/AssemblyInfo.cs
@@ -1,15 +1,15 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Bridge.Html5")]
-[assembly: AssemblyProduct("Bridge.Html5")]
-[assembly: AssemblyDescription("JavaScript implementation using Bridge.NET.")]
+[assembly: AssemblyTitle("Bridge")]
+[assembly: AssemblyProduct("Bridge")]
+[assembly: AssemblyDescription("Build Mobile, Web And Desktop Apps In C#. Run On Any Device Using JavaScript.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Object.NET, Inc.")]
 [assembly: AssemblyCopyright("Copyright 2008-2015 Object.NET, Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("d5956119-f924-47e1-96c2-4d98b832eb38")]
+[assembly: Guid("76ba3097-5b23-4900-bca9-2ba3bc0afcfe")]
 [assembly: AssemblyVersion("1.0")]
 [assembly: AssemblyFileVersion("1.0.0-beta")]
