e05b5558c7d6de1220f4229c6cfcd21e1ffda982
Geoffrey McGill <geoff@object.net>
2015-03-30 01:04:54 -0600

    Removed -beta flag from version number

--- Bridge/Properties/AssemblyInfo.cs
+++ Bridge/Properties/AssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: Guid("76ba3097-5b23-4900-bca9-2ba3bc0afcfe")]
 [assembly: AssemblyVersion("1.0")]
-[assembly: AssemblyFileVersion("1.0.0-beta")]
+[assembly: AssemblyFileVersion("1.0.0")]
