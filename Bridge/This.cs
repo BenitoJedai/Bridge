bae6894b84f38cac5806f6783093567de07ffbee
Geoffrey McGill <geoff@object.net>
2015-04-02 16:36:19 -0600

    Added This.Instance. Closes #108

---
+++ Bridge/This.cs
@@ -0,0 +1,18 @@
+﻿using Bridge;
+
+namespace Bridge
+{
+    [Ignore]
+    [Name("this")]
+    public static class This
+    {
+        [Template("this")]
+        public static ThisInstance Instance;
+    }
+
+    [Ignore]
+    [Name("this")]
+    public class ThisInstance
+    {
+    }
+}
