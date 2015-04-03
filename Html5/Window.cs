31fa7e0dfc2b29141e1536a45f1117ae538d1e55
Geoffrey McGill <geoff@object.net>
2015-04-02 21:09:10 -0600

    Add .Get .Set and .Call static Methods to Window class. Closes #110

--- Html5/Window.cs
+++ Html5/Window.cs
@@ -1636,5 +1636,33 @@ namespace Bridge.Html5
         {
             return null;
         }
+
+        [Template("{this}[{name}].call(null, {args})")]
+        public static void Call(string name, params object[] args)
+        {
+        }
+
+        [Template("{this}[{name}].call(null, {args})")]
+        public static T Call<T>(string name, params object[] args)
+        {
+            return default(T);
+        }
+
+        [Template("{this}[{name}]")]
+        public static object Get(string name)
+        {
+            return null;
+        }
+
+        [Template("{this}[{name}]")]
+        public static T Get<T>(string name)
+        {
+            return default(T);
+        }
+
+        [Template("{this}[{name}] = {value}")]
+        public static void Set(string name, object value)
+        {
+        }
     }
 }
